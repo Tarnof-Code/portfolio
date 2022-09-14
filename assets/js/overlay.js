042d48ca75ec8f1a55d9d341737d4b41ee92c7e2
Tarnof <tarnof@hotmail.com>
2022-09-14 16:23:58 +0200

    overlay video OK

---
+++ assets/js/overlay.js
@@ -0,0 +1,15 @@
+function on1() {
+    document.getElementById("overlay1").style.display = "block";
+}
+
+function off1() {
+    document.getElementById("overlay1").style.display = "none";
+}
+
+function on2() {
+    document.getElementById("overlay2").style.display = "block";
+}
+
+function off2() {
+    document.getElementById("overlay2").style.display = "none";
+}
