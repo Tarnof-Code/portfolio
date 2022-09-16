19eb1f307d94f0ab8f5296150db2f5716517c985
Tarnof <tarnof@hotmail.com>
2022-09-16 15:24:14 +0200

    projets OK

---
+++ assets/js/overlayMore.js
@@ -0,0 +1,46 @@
+function onMore1() {
+    document.getElementById("overlayMore1").style.display = "block";
+}
+function offMore1() {
+    document.getElementById("overlayMore1").style.display = "none";
+}
+
+
+function onMore2() {
+    document.getElementById("overlayMore2").style.display = "block";
+}
+function offMore2() {
+    document.getElementById("overlayMore2").style.display = "none";
+}
+
+
+function onMore3() {
+    document.getElementById("overlayMore3").style.display = "block";
+}
+function offMore3() {
+    document.getElementById("overlayMore3").style.display = "none";
+}
+
+
+function onMore4() {
+    document.getElementById("overlayMore4").style.display = "block";
+}
+function offMore4() {
+    document.getElementById("overlayMore4").style.display = "none";
+}
+
+
+function onMore5() {
+    document.getElementById("overlayMore5").style.display = "block";
+}
+function offMore5() {
+    document.getElementById("overlayMore5").style.display = "none";
+}
+
+
+function onMore6() {
+    document.getElementById("overlayMore6").style.display = "block";
+}
+function offMore6() {
+    document.getElementById("overlayMore6").style.display = "none";
+}
---
+++ assets/js/overlayVideo.js
@@ -0,0 +1,15 @@
+function onVideo1() {
+    document.getElementById("overlayVideo1").style.display = "block";
+}
+
+function offVideo1() {
+    document.getElementById("overlayVideo1").style.display = "none";
+}
+
+function onVideo2() {
+    document.getElementById("overlayVideo2").style.display = "block";
+}
+
+function offVideo2() {
+    document.getElementById("overlayVideo2").style.display = "none";
+}
